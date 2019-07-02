47175066bf0b39f2b5da8904732c32c0e3d8fe36
tianzhili <270700332@qq.com>
2019-07-02 17:16:09 +0800

    修改主页访问

--- src/main/java/com/book/tzl/hello/HelloController.java
+++ src/main/java/com/book/tzl/hello/HelloController.java
@@ -1,20 +1,16 @@
 package com.book.tzl.hello;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloController {
 
-	@Value("${name}")
-	private String name;
-
-	@Value("${age}")
-	private Integer age;
-
 	@RequestMapping("/hello")
-	public String hello() {
-		return name + age;
+	public String hello(Map<String, Object> map) {
+		map.put("user", "Tyrone");
+		return "index";
 	}
 }
