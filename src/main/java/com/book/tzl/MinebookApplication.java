9d3ba043f10c54b77869981ee1296613366470e4
tianzhili <270700332@qq.com>
2019-07-02 15:07:26 +0800

    创建我的记账本

---
+++ src/main/java/com/book/tzl/MinebookApplication.java
@@ -0,0 +1,13 @@
+package com.book.tzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinebookApplication.class, args);
+	}
+
+}
