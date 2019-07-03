c414ce63b1ca13f1a9274cb4522bc74385719d06
tianzhili <270700332@qq.com>
2019-07-03 14:07:57 +0800

    没啥要修改的

--- src/main/java/com/book/tzl/hello/HelloController.java
+++ src/main/java/com/book/tzl/hello/HelloController.java
@@ -1,16 +1,13 @@
 package com.book.tzl.hello;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hello")
-	public String hello(Map<String, Object> map) {
-		map.put("user", "Tyrone");
-		return "index";
+	@RequestMapping("/login")
+	public String loign() {
+		return "login/login";
 	}
 }
