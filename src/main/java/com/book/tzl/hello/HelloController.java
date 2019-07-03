2817eecf1a275f606879adfa9f3270044564e335
tianzhili <270700332@qq.com>
2019-07-03 17:28:22 +0800

    这个不清楚
    Signed-off-by: tianzhili <270700332@qq.com>

--- src/main/java/com/book/tzl/hello/HelloController.java
+++ src/main/java/com/book/tzl/hello/HelloController.java
@@ -1,13 +1,22 @@
 package com.book.tzl.hello;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.book.tzl.common.Version;
 
 @Controller
 public class HelloController {
 
+	@Autowired
+	private Version version;
+
 	@RequestMapping("/login")
-	public String loign() {
+	public String loign(Map<Object, String> map) {
+		map.put("versionName", version.getName());
 		return "login/login";
 	}
 }
