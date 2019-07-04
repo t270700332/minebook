125ef569b3679bad23327b73a8cfc077ee762ec4
tianzhili <270700332@qq.com>
2019-07-04 15:37:09 +0800

    结果

--- src/main/java/com/book/tzl/common/JsonResult.java
+++ src/main/java/com/book/tzl/common/JsonResult.java
@@ -21,6 +21,30 @@ public class JsonResult {
 		this.data = data;
 	}
 
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Map<String, Object> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, Object> data) {
+		this.data = data;
+	}
+
 	public static String success() {
 		return success(new HashMap<>(0));
 	}
